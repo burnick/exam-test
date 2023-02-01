f41d6c476f614b597c8371bcb425cc4ff5581b74
burnick <arnelceledonio@gmail.com>
2023-02-01 08:25:42 +0800

    fix: typo for tests

--- src/pages/Login.test.tsx
+++ src/pages/Login.test.tsx
@@ -35,11 +35,7 @@ test('test Branch Id Input  for Number input', async () => {
   
 
   await act(async () => {
-    const { getByPlaceholderText } = await render(
-      <TestComponent>
-        <Login />
-      </TestComponent>,
-    );
+   
     const inputNode = getByPlaceholderText('Branch Id');
     fireEvent.change(inputNode, { target: { value: '2323' } });
     expect((inputNode as HTMLInputElement).value).toBe('2323');
@@ -55,28 +51,23 @@ test('test Branch Id Input  for Letter input', async () => {
   
 
   await act(async () => {
-    const { getByPlaceholderText } = await render(
-      <TestComponent>
-        <Login />
-      </TestComponent>,
-    );
     const inputNode = getByPlaceholderText('Branch Id');
     fireEvent.change(inputNode, { target: { value: 'abcde' } });
     expect((inputNode as HTMLInputElement).value).toBe('');
   });
 });
 
-test('test Button submit for empty form', async () => {
+test('submit empty form, test Button submit should be disabled', async () => {
   render(
     <TestComponent>
       <Login />
     </TestComponent>,
   );
   const user = userEvent.setup();
-
+  const buttonNode = screen.getByRole('button', { name: /login/i });
   await user.type(screen.getByPlaceholderText('Branch Id'), 'John');
-  await user.click(screen.getByRole('button', { name: /login/i }));
+  await user.click(buttonNode);
   await waitFor(() =>
-    expect(screen.getByRole('button', { name: /login/i })).toBeDisabled(),
+    expect(buttonNode).toBeDisabled(),
   );
 });
