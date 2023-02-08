8a415115e9ff9995a8399c90aa2ac5ad57cd87e7
burnick <arnelceledonio@gmail.com>
2023-02-08 12:48:46 +0800

    fix: typo

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './slice/users';
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./slice/users";
 
 export const store = configureStore({
   reducer: userReducer,
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: true,
     }),
   ],
-  devTools: process.env.NODE_ENV !== 'production',
+  devTools: process.env.NODE_ENV !== "production",
 });
-
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
