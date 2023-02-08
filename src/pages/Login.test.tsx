376f49e1010dc98afb9d541d45112be83983d54c
burnick <arnelceledonio@gmail.com>
2023-02-08 18:41:58 +0800

    fix: refactor test

--- src/pages/Login.test.tsx
+++ src/pages/Login.test.tsx
@@ -2,24 +2,25 @@ import { cleanup, render, fireEvent, act, waitFor, screen } from '@testing-libra
 import TestComponent from 'components/TestComponent';
 import userEvent from '@testing-library/user-event';
 import Login from './Login';
-
-beforeAll(() => {});
-afterEach(cleanup);
-afterAll(() => {});
-
-test('renders Login Component', async () => {
-  const { getByRole, getByPlaceholderText } = await render(
+beforeEach(async () => {
+  await render(
     <TestComponent>
       <Login />
     </TestComponent>,
   );
-  const buttonElements = getByRole('button', {
+});
+//beforeAll(() => {});
+afterEach(cleanup);
+//afterAll(() => {});
+
+test('renders Login Component', async () => {
+  const buttonElements = screen.getByRole('button', {
     name: /login/i,
   });
 
-  const branchIdElement = getByPlaceholderText('Branch Id');
-  const userNameElement = getByPlaceholderText('User name');
-  const passwordElement = getByPlaceholderText('Password');
+  const branchIdElement = screen.getByPlaceholderText('Branch Id');
+  const userNameElement = screen.getByPlaceholderText('User name');
+  const passwordElement = screen.getByPlaceholderText('Password');
 
   expect(buttonElements).toBeVisible();
   expect(branchIdElement).toBeVisible();
@@ -28,12 +29,7 @@ test('renders Login Component', async () => {
 });
 
 test('test Branch Id Input for Number input', async () => {
-  const { getByPlaceholderText } = render(
-    <TestComponent>
-      <Login />
-    </TestComponent>,
-  );
-  const inputNode = getByPlaceholderText('Branch Id');
+  const inputNode = screen.getByPlaceholderText('Branch Id');
   fireEvent.change(inputNode, { target: { value: '2323' } });
   await act(async () => {
     expect((inputNode as HTMLInputElement).value).toBe('2323');
@@ -41,12 +37,7 @@ test('test Branch Id Input for Number input', async () => {
 });
 
 test('test Branch Id Input for Letter input', async () => {
-  const { getByPlaceholderText } = render(
-    <TestComponent>
-      <Login />
-    </TestComponent>,
-  );
-  const inputNode = getByPlaceholderText('Branch Id');
+  const inputNode = screen.getByPlaceholderText('Branch Id');
   fireEvent.change(inputNode, { target: { value: 'abcde' } });
   await act(async () => {
     expect((inputNode as HTMLInputElement).value).toBe('');
@@ -55,17 +46,12 @@ test('test Branch Id Input for Letter input', async () => {
 
 test('test User name Input for empty', async () => {
   const errorMessageUserName = 'Error: userName is a required field';
-  const { getByPlaceholderText, getByText } = render(
-    <TestComponent>
-      <Login />
-    </TestComponent>,
-  );
-  const user = userEvent.setup();
 
-  const inputBranchId = getByPlaceholderText('Branch Id');
+  const user = userEvent.setup();
+  const inputBranchId = screen.getByPlaceholderText('Branch Id');
   fireEvent.change(inputBranchId, { target: { value: '123456' } });
   const buttonNode = screen.getByRole('button', { name: /login/i });
-  const inputUsername = getByPlaceholderText('User name');
+  const inputUsername = screen.getByPlaceholderText('User name');
   fireEvent.change(inputUsername, { target: { value: '' } });
 
   await user.click(buttonNode);
@@ -78,11 +64,6 @@ test('test User name Input for empty', async () => {
 });
 
 test('submit empty form, test Button submit should be disabled', async () => {
-  render(
-    <TestComponent>
-      <Login />
-    </TestComponent>,
-  );
   const user = userEvent.setup();
   const buttonNode = screen.getByRole('button', { name: /login/i });
   await user.type(screen.getByPlaceholderText('Branch Id'), 'John');
